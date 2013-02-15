4bb7ab97f97cba14f9ff814e7fe8e4c3fee5130a
Expez <mrgrandmother@gmail.com>
2013-02-15 13:57:16 +0100

    Make configuration a singleton and add new settings.

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -5,36 +5,37 @@ require 'configtoolkit/keyvaluereader'
 describe 'Configuration' do
 
   before :all do
+    CONFIG_FILE = File.expand_path_relative_to_caller("../hatterrc")
     @maildir_path = "/path/to/maildir"
-    CONFIG_FILE = File.expand_path_relative_to_caller("../.hatterrc")
     @maildir_format = "maildir"
-    @config = Configuration.new() do |config|
-      config.maildir = @maildir_path
-      config.maildir_format = @maildir_format
-    end
-  end
-
-  it "contains all the required configuration settings" do
-    @config.maildir?.should be_true
-    @config.maildir_format?.should be_true
-  end
-
-  it "the settings have the correct values" do
-    @config.maildir.should eq @maildir_path
-    @config.maildir_format.should eq @maildir_format
-  end
-
-  it "reads the configuration values from file" do
-    config = Configuration.from_file CONFIG_FILE
-    config.maildir.should eq @maildir_path
-    config.maildir_format.should eq @maildir_format
+    @termbox_library_path = "/usr/lib/libtermbox.so"
   end
 
   it "barfs when required values are missing" do
     config = File.read CONFIG_FILE
     config.gsub!(/maildir/, 'asdf')
     File.open("invalid_config", "w") {|file| file.write config}
-    expect {Configuration.from_file("invalid_config")}.to raise_error
+    expect {Configuration.instance "invalid_config"}.to raise_error
     FileUtils::rm("invalid_config")
   end
+
+  it "reads the configuration values from file" do
+    config = Configuration.instance
+    config.maildir.should eq @maildir_path
+    config.maildir_format.should eq @maildir_format
+  end
+
+  it "the settings have the correct values" do
+    config = Configuration.instance
+    config.maildir.should eq @maildir_path
+    config.maildir_format.should eq @maildir_format
+    config.termbox_library_path.should eq @termbox_library_path
+  end
+
+  it "contains a nested config with colors" do
+    config = Configuration.instance
+    config.colors.foreground.should eq "green"
+    config.colors.foreground.should eq "green"
+  end
+
 end
