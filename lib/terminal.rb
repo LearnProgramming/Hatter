c7fac3305a81f6ffc2d48cb4df36108bd896cc5d
Expez <mrgrandmother@gmail.com>
2013-02-16 12:44:45 +0100

    Implement terminal interface

---
+++ lib/terminal.rb
@@ -0,0 +1,48 @@
+module Termbox
+  TB_INPUT_ESCAPE = 1
+  TB_INPUT_ALT = 2
+end
+
+class Terminal
+  require 'termbox'
+  require 'views/mail_view'
+  require 'views/label_view'
+  require 'views/folder_view'
+
+  def initialize
+    initialize_termbox
+    initialize_views
+  end
+
+  def draw
+    Termbox.tb_clear
+    @views.each {|view| view.draw}
+    Termbox.tb_present
+  end
+
+  def shutdown
+    Termbox.tb_shutdown
+  end
+
+  private
+
+  def initialize_views
+    folder_view_location = {x0: 0, y0: 0, x1: 19, y1: 40}
+    label_view_location = {x0: 0, y0: 41, x1: 19, y1: 79}
+    mail_view_location = {x0: 20, y0: 0, x1: 79, y1: 79}
+    @views = [Mail_view.new(mail_view_location),
+              Label_view.new(label_view_location),
+              Folder_view.new(folder_view_location)]
+  end
+
+  def initialize_termbox
+    libtermbox_path = Configuration.instance.termbox_library_path
+    Termbox.initialize_library libtermbox_path
+    unless Termbox.tb_init
+      raise "Failed failed to start UI"
+    end
+    @event = Termbox::Event.new
+    Termbox.tb_select_input_mode Termbox::TB_INPUT_ESCAPE
+  end
+
+end
